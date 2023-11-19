172cbd24658c7723f2cc6d6408bc3e38141c462b
ASGladchenko <asgladchenko@gmail.com>
2023-11-19 09:39:00 +0200

    added randomazier to buns_btn

--- front/src/components/buns-banner/buns-btn/index.tsx
+++ front/src/components/buns-banner/buns-btn/index.tsx
@@ -1,36 +1,35 @@
 import clsx from 'clsx';
+
 import { useRotationControls } from '@/hooks';
 import Right from '@/assets/svg/right.svg?react';
 
 import { getStyles } from './styles';
+import { getRandomNumber } from '../helpers';
 
 export const BunsBtn = ({
-  className,
   left,
+  className,
 }: {
-  className?: string;
   left?: boolean;
+  className?: string;
 }) => {
-  const { after, before } = getStyles();
+  const { after, before, btn, icon } = getStyles(className);
   const { deg: afterDeg, handleClick: afterClick } = useRotationControls({
-    initialDeg: 113,
+    initialDeg: getRandomNumber({ min: 113, max: 212 }),
   });
   const { deg: beforeDeg, handleClick: beforeClick } = useRotationControls({
-    initialDeg: 212,
     maxIterations: 6,
     startDirection: 'down',
+    initialDeg: getRandomNumber({ min: 87, max: 193 }),
   });
 
   const onClick = () => {
-    afterClick(174);
-    beforeClick(114);
+    afterClick(getRandomNumber({ min: 132, max: 193 }));
+    beforeClick(getRandomNumber({ min: 87, max: 164 }));
   };
 
   return (
-    <button
-      onClick={onClick}
-      className={`relative w-[60px] h-[60px] group bg-mainGradient rounded-full cursor-pointer shadow-md shadow-borderP active:scale-90 transition duration-150 ${className}`}
-    >
+    <button onClick={onClick} className={btn}>
       <div
         className={after}
         style={{ transform: `translate(-50%,-50%) rotate(${afterDeg}deg)` }}
@@ -39,7 +38,7 @@ export const BunsBtn = ({
         className={before}
         style={{ transform: `translate(-50%,-50%) rotate(${beforeDeg}deg)` }}
       ></div>
-      <div className="absolute w-6 h-6 text-white transition duration-200 -translate-x-1/2 -translate-y-1/2 group-hover:scale-110 top-1/2 left-1/2">
+      <div className={icon}>
         <Right className={clsx({ 'rotate-180': left })} />
       </div>
     </button>
--- front/src/components/buns-banner/buns-btn/styles.ts
+++ front/src/components/buns-banner/buns-btn/styles.ts
@@ -1,10 +1,15 @@
 import clsx from 'clsx';
 
-export const getStyles = () => ({
+export const getStyles = (className?: string) => ({
+  btn: clsx(
+    `relative w-[60px] h-[60px] group bg-mainGradient rounded-full cursor-pointer shadow-md shadow-borderP active:scale-90 transition duration-150) `,
+    className,
+  ),
   after: clsx(
     'transition duration-300 delay-100 absolute block w-[98%] h-[98%] rounded-[50%] border-bunsBorder1 border-[4.5px]  border-b-transparent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
   ),
   before: clsx(
     'transition duration-300 absolute block w-[75%] h-[75%] rounded-[50%] border-bunsBorder border-[4px]  border-b-transparent top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
   ),
+  icon: 'absolute w-6 h-6 text-white transition duration-200 -translate-x-1/2 -translate-y-1/2 group-hover:scale-110 top-1/2 left-1/2',
 });
