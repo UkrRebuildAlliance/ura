a8e7f79f5ec54311faf32d9163fc69f2031de789
ASGladchenko <asgladchenko@gmail.com>
2023-12-19 10:20:58 +0200

    fixed mockup

--- front/src/i18n/en/index.ts
+++ front/src/i18n/en/index.ts
@@ -35,7 +35,7 @@ export const en = {
         viewAllTeam: 'View all',
     },
     main_banner: {
-        ttl: 'The Ukraine Recovery Alliance — is the main provider of international assistance.',
+        ttl: `The Ukraine Rebuilding Alliance — is the main provider of international assistance.`,
         mission:
             'Our mission — create conditions for Ukrainians to return home.',
     },
